8defe79f40a3e6b4fcfe4d3186263defbd37444c
ChaimaGharbi <Chaima.GH6602@gmail.com>
2025-12-04 20:09:54 +0100

    initial commité

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to My App!</h1>
+      <p>This is a simple React app deployed with GitOps.</p>
+    </div>
+  );
+}
+
+export default App;
