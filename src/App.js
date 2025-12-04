e1f0b61ec2790cdd1cb3a536ee163c341c94d470
ChaimaGharbi <Chaima.GH6602@gmail.com>
2025-12-04 21:11:54 +0100

    added github workflow

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div style={{textAlign: 'center', marginTop: '50px'}}>
       <h1>Welcome to My App!</h1>
       <p>This is a simple React app deployed with GitOps.</p>
+      <p>This is after editing to see it successuly automatically syncs</p>
     </div>
   );
 }
